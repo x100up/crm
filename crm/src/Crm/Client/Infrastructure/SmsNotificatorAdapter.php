b8ca4b96a5a990d36f2d1d61c44163346a0c89e5
rpavlenko <pavlenko.roman.spb@gmail.com>
2019-12-10 23:31:07 +0300

    Add sms and email infrastructure Create NotificatorService for client Connect MailSender and SmsSender to NotificatorService through adapters

---
+++ crm/src/Crm/Client/Infrastructure/SmsNotificatorAdapter.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Crm\Client\Infrastructure;
+
+use App\Crm\Client\Domain\Model\Client;
+use App\Crm\Client\Interfaces\ClientNotificatorInterface;
+use App\Infrastructure\SmsSender\SmsSender;
+
+class SmsNotificatorAdapter implements ClientNotificatorInterface
+{
+    /** @var SmsSender */
+    private $smsSender;
+
+    public function __construct(SmsSender $smsSender)
+    {
+        $this->smsSender = $smsSender;
+    }
+
+    public function notifyClient(Client $client, string $message): void
+    {
+        $this->smsSender->sendSms($client->getPhone(), $message);
+    }
+}
---
+++ crm/src/Infrastructure/SmsSender/SmsSender.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infrastructure\SmsSender;
+
+class SmsSender
+{
+    public function sendSms(string $phoneNumber, string $message): void {
+        // send sms
+    }
+}
