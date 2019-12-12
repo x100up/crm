5323154add43f8ff7b8e1ade21b0bda748e752ef
rpavlenko <pavlenko.roman.spb@gmail.com>
2019-12-12 21:02:01 +0300

    Add authenticators by token

--- crm/src/Crm/Users/Domain/Model/User.php
+++ crm/src/Crm/Users/Domain/Model/User.php
@@ -44,4 +44,14 @@ class User {
     {
         return $this->id;
     }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
 }
